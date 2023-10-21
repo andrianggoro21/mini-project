22d5216293e7d85d4eecb36eb884edcc0f939335
andrianggoro21 <andri.anggoro21@gmail.com>
2023-10-21 17:20:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import Home from "./pages/home";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
-import Dashboard from "./pages/dashboard";
-import CreateEvent from "./pages/createEvent";
-import CreateTicket from "./pages/createTicket";
-import { Route, Routes } from "react-router-dom";
-import ListEvent from "./components/listEvent";
+
 // import logo from './logo.svg';
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
@@ -13,6 +6,13 @@ import Event from "./pages/event";
 import Waiting from "./pages/transaction/waiting";
 import Success from "./pages/transaction/success";
 import Attedance from "./pages/eventAttedance";
+import Home from "./pages/home";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Dashboard from "./pages/dashboard";
+import CreateEvent from "./pages/createEvent";
+import CreateTicket from "./pages/createTicket";
+import ListEvent from "./components/listEvent";
 
 
 function App() {
@@ -23,14 +23,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/event-form" element={<CreateEvent/>}/>
-      </Routes>
-      <Footer />
-      <Routes>
         <Route path="/event" element={<Event/>}/>
         <Route path="/transaction/waiting" element={<Waiting/>}/>
         <Route path="/transaction/success" element={<Success/>}/>
         <Route path="/attedance" element={<Attedance/>}/>
       </Routes>
+      <Footer />
     </>
   );
 }
